736c6d81412685099f9b5aaeee879292aeb5dfb2
Rupan Samanta <rvsjan32@gmail.com>
2026-08-24 10:14:58 +0530

    Refactor Footer and Header components for improved layout and styling; update index.css for responsive design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,14 +2,14 @@ import { ShoppingBag, Wallet } from "lucide-react"
 import FooterButton from "./FooterButton"
 
 const Footer = () => {
-    
+
     const buttonList = [
         { icon: Wallet, label: 'Overview' },
         { icon: ShoppingBag, label: 'Purchases' }
     ];
     
     return (
-        <div className="w-full fixed bottom-0 flex justify-evenly bg-[#14233f] p-5 text-sm text-gray-200 rounded-tl-3xl rounded-tr-3xl">
+        <div className="absolute inset-x-0 bottom-0 flex w-full justify-evenly rounded-tl-3xl rounded-tr-3xl bg-[#14233f] p-5 text-sm text-gray-200">
             {buttonList.map((obj) => (
                 <FooterButton key={obj.label} Icon={obj.icon} label={obj.label} />
             ))}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,21 +2,21 @@ import { Avatar, AvatarFallback } from "../ui/avatar"
 
 const Header = () => {
     const name = "Rupan";
-    const amount = 0;
+    const amount = 100;
     const raised = 0;
     return (
-        <div className="w-full fixed top-0 bg-[#14233f] flex justify-between flex-wrap px-7 p-5 text-gray-200 rounded-bl-3xl rounded-br-3xl">
-            <h3 className="text-amber-400">DABBA FUND</h3>
+        <div className="w-full bg-[#14233f] flex justify-between flex-wrap px-7 p-5 text-gray-200 rounded-bl-3xl rounded-br-3xl">
+            <h2 className="text-amber-400">DABBA FUND</h2>
             <div className="flex items-center gap-2 text-sm p-1 pr-2 border-[0.05rem] border-gray-300 bg-gray-500/10 rounded-full">
                 <Avatar size="sm">
                     <AvatarFallback>{name[0]}</AvatarFallback>
                 </Avatar>
                 <span>{name}</span>
             </div>
-            <div className="w-full space-y-3 mt-3 font-light">
+            <div className="w-full space-y-3 mt-3 font-light text-left">
                 <h3 className="text-5xl font-normal">₹{amount}</h3>
                 <div>in the fund right now</div>
-                <div className="flex gap-5 justify-start text-gray-400 text-md">
+                <div className="flex gap-5 justify-start text-gray-400 text-lg">
                     <span>₹{raised} raised</span>
                     <span>₹{raised} spent</span>
                 </div>
