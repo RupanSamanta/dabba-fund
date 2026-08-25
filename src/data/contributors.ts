67832925c58feeb0533eaa513beebb792b27f334
Rupan Samanta <rvsjan32@gmail.com>
2026-08-25 16:30:03 +0530

    fix: remove isCurrentUser field from contributors and ensure proper formatting

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -9,7 +9,6 @@ export const defaultContributors: Contributor[] = [
     initials: "RU",
     amount: 0,
     isAdmin: true,
-    isCurrentUser: false,
     avatarBgColor: "bg-[#b08238] text-white border-2 border-[#b08238]",
     subtext: ""
   },
@@ -19,6 +18,17 @@ export const defaultContributors: Contributor[] = [
     name: "Souvagya",
     fullname: "Souvagya Sarkar",
     initials: "SS",
+    amount: 0,
+    isAdmin: false,
+    avatarBgColor: "bg-[#f5fb5d] text-white",
+    subtext: ""
+  } ,
+  {
+    id: "20050103",
+    email: "arpanpurkait550@gmail.com",
+    name: "Arpan",
+    fullname: "Arpan Purkait",
+    initials: "AP",
     amount: 50,
     isAdmin: false,
     avatarBgColor: "bg-[#f5fb5d] text-white",
